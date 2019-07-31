5d1c4671ccfa0a93b4b7ec70bc1e81b4d1e37389
Aaron Trovillion <aaron.trovillion@gmail.com>
2019-07-31 13:45:40 -0400

    Add routing controllers view checkpoint work

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -7,33 +7,13 @@ describe("routes : static", () => {
 
     describe("GET /", () => {
 
-        it("should return status code 200", (done) => {
+        it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", (done) => {
 
             request.get(base, (err, res, body) => {
                 expect(res.statusCode).toBe(200);
-
+                expect(body).toContain("Welcome to Bloccit");
                 done();
             });
         });
     });
-
-    describe("GET /marco", () => {
-        it("should return status code 200", (done) => {
-
-            request.get(marco, (err, res, body) => {
-                expect(res.statusCode).toBe(200);
-
-                done();
-            })
-        });
-
-        it("should return the word polo", (done) => {
-
-            request.get(marco, (err, res, body) => {
-                expect(body).toBe("polo!");
-
-                done();
-            })
-        })
-    })
 });
