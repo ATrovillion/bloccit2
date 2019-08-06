b7701e8adb80efe5ec673b95f8fb1b694e037ee2
ATrovillion <aaron.trovillion@gmail.com>
2019-08-06 16:33:54 -0400

    Add completed topic resource 2 assignment; all working

--- spec/integration/ad_spec.js
+++ spec/integration/ad_spec.js
@@ -80,4 +80,77 @@ describe("routes : ads", () => {
             );
         });
     });
+
+    describe("GET /ads/:id", () => {
+
+        it("should render a view with the selected ad", (done) => {
+            request.get(`${base}${this.ad.id}`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("Colgate");
+                done();
+            });
+        });
+    });
+
+    describe("POST /ads/:id/destroy", () => {
+
+        it("should delete the ad with the associated ID", (done) => {
+
+            Ad.all()
+            .then((ads) => {
+
+                const adCountBeforeDelete = ads.length;
+
+                expect(adCountBeforeDelete).toBe(1);
+
+                request.post(`${base}${this.ad.id}/destroy`, (err, res, body) => {
+                    Ad.all()
+                    .then((ads) => {
+                        expect(err).toBeNull();
+                        expect(ads.length).toBe(adCountBeforeDelete - 1);
+                        done();
+                    });
+                });
+            });
+        });
+    });
+
+    describe("GET /ads/:id/edit", () => {
+
+        it("should render a view with an edit ad form", (done) => {
+            request.get(`${base}${this.ad.id}/edit`, (err, res, body) => {
+                expect(err).toBeNull();
+                expect(body).toContain("Edit Ad");
+                expect(body).toContain("Colgate");
+                done();
+            })
+        });
+    });
+
+    describe("POST /ads/:id/update", () => {
+
+        it("should update the ad with the given values", (done) => {
+            const options = {
+                url: `${base}${this.ad.id}/update`,
+                form: {
+                    title: "Colgate Toothpaste",
+                    description: "toothpaste"
+                }
+            };
+
+            request.post(options,
+                (err, res, body) => {
+
+                    expect(err).toBeNull();
+
+                    Ad.findOne({
+                        where: { id: this.ad.id }
+                    })
+                    .then((ad) => {
+                        expect(ad.title).toBe("Colgate Toothpaste");
+                        done();
+                    });
+                });
+        })
+    })
 })
--- src/db/queries.ads.js
+++ src/db/queries.ads.js
@@ -24,5 +24,46 @@ module.exports = {
         .catch((err) => {
             callback(err);
         })
+    },
+
+    getAd(id, callback){
+        return Ad.findById(id)
+        .then((ad) => {
+            callback(null, ad);
+        })
+        .catch((err) => {
+            callback(err);
+        })
+    },
+
+    deleteAd(id, callback){
+        return Ad.destroy({
+            where: {id}
+        })
+        .then((ad) => {
+            callback(null, ad);
+        })
+        .catch((err) => {
+            callback(err);
+        })
+    },
+
+    updateAd(id, updatedAd, callback){
+        return Ad.findById(id)
+        .then((ad) => {
+            if(!ad){
+                return callback("Ad not found");
+            }
+
+            ad.update(updatedAd, {
+                fields: Object.keys(updatedAd)
+            })
+            .then(() => {
+                callback(null, ad);
+            })
+            .catch((err) => {
+                callback(err);
+            })
+        })
     }
 }
